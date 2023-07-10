fc2657ce4c92e06d9a6290312fbabe65ae51cb85
chaseknis <chase6th@gmail.com>
2023-07-10 10:49:24 +0200

    Install proptypes and fix linters

--- src/components/WorkItems.js
+++ src/components/WorkItems.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const WorkItems = ({ item }) => {
+const WorkItems = ({ item }) => (
+  <div className="work_card" key={item.id}>
 
-  return(
-    <div className="work_card" key={item.id}>
+    <img src={item.image} alt="" className="work_image" />
+    <h3 className="work_title">{item.title}</h3>
+    <a href="https://car-rental-capstone.netlify.app/" className="work_button button">
+      Demo
+      <i className="bx bx-right-arrow-alt work_button_icon" />
+    </a>
 
-       <img src={item.image}  alt="" className="work_image"></img>
-       <h3 className="work_title">{item.title}</h3>
-       <a href="#" className="work_button button">
-          Demo
-          <i class="bx bx-right-arrow-alt work_button_icon"></i>
-        </a>
+  </div>
+);
 
-    </div>
-    );
+WorkItems.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default WorkItems;
--- src/components/Works.js
+++ src/components/Works.js
@@ -3,44 +3,55 @@ import { projectsData, projectsNav } from './Data';
 import WorkItems from './WorkItems';
 
 const Works = () => {
-    const [item, setItem] = useState({name: 'all' });
-    const [projects, setProjects] = useState([]);
-    const [active, setActive] = useState(0);
+  const [item, setItem] = useState({ name: 'all' });
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
 
-    useEffect(() => {
-        if (item.name === "all") {
-            setProjects(projectsData);
-        } else {
-            const newProjects = projectsData.filter((project) => {
-                return project.category ===item.name;
-            });
-            setProjects(newProjects);
-        }
-    }, [item]);
+  useEffect(() => {
+    if (item.name === 'all') {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter(
+        (project) => project.category.toLowerCase() === item.name,
+      );
+      setProjects(newProjects);
+    }
+  }, [item]);
 
-    const handleClick = (e, index) => {
-       setItem({ name: e.target.textContent });
-       setActive(index);
-    };
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent.toLowerCase() });
+    setActive(index);
+  };
 
-return (
+  return (
     <div>
-        <div className="work_filters">
-            {projectsNav.map((item, index) => {
-                return <span onClick={(e) => {
-                    handleClick(e, index);
-                }} className={`${active === index ? 'active_work' : ""} work_item`} key={index}>{item.name}</span>
-            })}
-        </div>
+      <div className="work_filters">
+        {projectsNav.map((item, index) => (
+          <span
+            onClick={(e) => {
+              handleClick(e, index);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleClick(e, index);
+              }
+            }}
+            role="button"
+            tabIndex={0}
+            className={`${active === index ? 'active_work' : ''} work_item`}
+            key={index}
+          >
+            {item.name}
+          </span>
 
-        <div className="work_group">
-            {projects.map((item) => {
-                return <WorkItems item={item} key={item.id} />
-            })}
-        </div>
+        ))}
+      </div>
+
+      <div className="work_group">
+        {projects.map((item) => <WorkItems item={item} key={item.id} />)}
+      </div>
     </div>
-);
-  
+  );
 };
 
 export default Works;
