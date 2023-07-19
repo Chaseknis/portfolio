bdc02922c8db0fd83a8ff514c2bafb381cf8f03b
chaseknis <chase6th@gmail.com>
2023-07-19 02:01:12 +0200

    Add footer

--- src/routes/Footer.js
+++ src/routes/Footer.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import './styles/footer.css';
 
-function Footer() {
-  return (
-    <h1>Footer</h1>
-  );
-}
+const Footer = () => (
+  <footer>
+    <div className="footer_container">
+      <a href="/" className="logo footer_logo">Chase</a>
+      <ul>
+        <li>
+          <a href="/" className="footer_link">About</a>
+        </li>
+
+        <li>
+          <a href="/" className="footer_link">Projects</a>
+        </li>
+
+        <li>
+          <a href="/" className="footer_link">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials_wrapper">
+        <a href="https://github.com/Chaseknis" className="socials_icon socials-1" target="_blank" rel="noreferrer">
+          <i className="uil uil-github" />
+        </a>
+
+        <a href="https://www.linkedin.com/in/innocent-n/" className="socials_icon socials-2" target="_blank" rel="noreferrer">
+          <i className="uil uil-linkedin" />
+        </a>
+
+        <a href="https://twitter.com/chasetotheworld" className="socials_icon socials-3" target="_blank" rel="noreferrer">
+          <i className="uil uil-twitter" />
+        </a>
+
+        <a href="https://www.instagram.com/chasetotheworld/" className="socials_icon socials-3" target="_blank" rel="noreferrer">
+          <i className="uil uil-instagram-alt" />
+        </a>
+      </div>
+
+      <span className="copyrights">
+        &#169; 2023 Chase. All rights reserved
+      </span>
+
+    </div>
+  </footer>
+);
 
 export default Footer;
